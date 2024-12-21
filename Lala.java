83ed608d03d932ceeb6562bd84c94de894502943
Tousart <150588467+Tousart@users.noreply.github.com>
2024-12-22 00:50:49 +0300

    Add files via upload

---
+++ Lala.java
@@ -0,0 +1,12 @@
+package Lab15_Calculator;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Lala {
+    public static void main(String[] args) {
+        double a = 3;
+        double b = 2.7;
+        System.out.println(a - b);
+    }
+}
